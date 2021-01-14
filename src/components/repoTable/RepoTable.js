3cc8fcaa687c065c5ecb9cb2ca027d2b527f503b
Benjamin Gagne <benanater01@gmail.com>
2021-01-14 01:10:17 -0500

    ISO8601 to human date, comments

--- src/components/repoTable/RepoTable.js
+++ src/components/repoTable/RepoTable.js
@@ -5,8 +5,9 @@ import Table from 'react-bootstrap/Table';
 function RepoTable() {
     const [repos, setRepos] = useState([]);
 
+    // fetch github repo sorted by updated date and store in repos
     useEffect(() => {
-        fetch("https://api.github.com/users/benngagne/repos")
+        fetch("https://api.github.com/users/benngagne/repos?sort=updated")
             .then(res => res.json())
             .then(
                 (result) => {
@@ -15,6 +16,37 @@ function RepoTable() {
             )
     }, [])
 
+    // function to convert ISO8601 to a human readable format
+    function isoTimeToHumanTime(isodate) {
+        var date = new Date(isodate);
+        var year = date.getFullYear();
+        var month = date.getMonth()+1;
+        var dt = date.getDate();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+
+        if (dt < 10) {
+            dt = '0' + dt;
+          }
+          if (month < 10) {
+            month = '0' + month;
+          }
+
+        return year + '-' + month + '-' + dt + ' ' + hours + ':' + minutes + ' ' + ampm;
+    }
+
+    // update entire repos array dates to human readable format
+    var i;
+    for(i=0; i < repos.length; i++) {
+        repos[i].updated_at = isoTimeToHumanTime(repos[i].updated_at);
+        repos[i].created_at = isoTimeToHumanTime(repos[i].updated_at);
+    }
+
+    // return in bootstrap table
     return (
         <Table striped bordered hover variant="dark">
             <thead>
@@ -28,7 +60,7 @@ function RepoTable() {
             <tbody>
                 {repos.map(repo => (
                     <tr>
-                        <a href={repo.html_url} target="_blank"><td key={repo.id}>{repo.name}</td></a>
+                        <a href={repo.html_url} target="_blank" rel="noreferrer"><td key={repo.id}>{repo.name}</td></a>
                         <td>{repo.description}</td>
                         <td>{repo.updated_at}</td>
                         <td>{repo.created_at}</td>
